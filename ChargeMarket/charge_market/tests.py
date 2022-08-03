bb32c0d0ede6548c360c30716039cbd7018e5136
AhmadZafarani <ahmadzafarani2013@gmail.com>
2022-08-03 16:41:10 +0430

    unit tests for add vendor

--- ChargeMarket/charge_market/tests.py
+++ ChargeMarket/charge_market/tests.py
@@ -1,3 +1,4 @@
+from django.db.transaction import atomic
 from django.test import TestCase
 
 from .models import *
@@ -11,7 +12,46 @@ class VendorModelTest(TestCase):
         self.assertEqual(str(vendor), f"vendor with id: 1 and credit: 200")
 
 
-class ProjectTests(TestCase):
-    def test_homepage(self):
-        response = self.client.get('/')
+class AddVendorTests(TestCase):
+    def test_add_vendor(self):
+        response = self.client.get('/addVendor?credit=21')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(Vendor.objects.count(), 1)
+        vendor = Vendor.objects.all()[0]
+        self.assertEqual(vendor.identifier, 1)
+        self.assertEqual(vendor.credit, 21)
+
+    def test_add_multiple_vendors(self):
+        response = self.client.get('/addVendor?credit=21')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Vendor.objects.count(), 1)
+        vendor = Vendor.objects.all()[0]
+        self.assertEqual(vendor.identifier, 1)
+        self.assertEqual(vendor.credit, 21)
+
+        response = self.client.get('/addVendor?credit=25')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Vendor.objects.count(), 2)
+        vendor = Vendor.objects.all()[1]
+        self.assertEqual(vendor.identifier, 2)
+        self.assertEqual(vendor.credit, 25)
+
+    def test_add_vendor_negative_credit(self):
+        with atomic():
+            response = self.client.get('/addVendor?credit=-21')
+        self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(Vendor.objects.count(), 0)
+
+    def test_add_vendor_zero_credit(self):
+        response = self.client.get('/addVendor?credit=0')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Vendor.objects.count(), 1)
+        vendor = Vendor.objects.all()[0]
+        self.assertEqual(vendor.identifier, 1)
+        self.assertEqual(vendor.credit, 0)
+
+    def test_add_vendor_float_credit(self):
+        with atomic():
+            response = self.client.get('/addVendor?credit=1.2')
+        self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(Vendor.objects.count(), 0)
--- ChargeMarket/charge_market/views.py
+++ ChargeMarket/charge_market/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpRequest, HttpResponse
+from django.db import IntegrityError
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+
+from .models import *
 
 
 def add_vendor(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello world!")
+    credit = request.GET.get("credit")
+    if credit is None:
+        return HttpResponseBadRequest("credit not found!")
+    vendor = Vendor()
+    error_string = "credit must be a positive Integer!"
+    try:
+        vendor.credit = int(credit)
+    except ValueError:
+        return HttpResponseBadRequest(error_string)
+    try:
+        vendor.save()
+    except IntegrityError:
+        return HttpResponseBadRequest(error_string)
+    return HttpResponse()
