a7fe0e8f9d78e1ddfa759e120fef17ad070ee3a0
AhmadZafarani <ahmadzafarani2013@gmail.com>
2022-08-22 10:15:07 +0430

    change database to mysql

--- ChargeMarket/ChargeMarket/settings.py
+++ ChargeMarket/ChargeMarket/settings.py
@@ -101,8 +101,15 @@ WSGI_APPLICATION = 'ChargeMarket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'charge_market',
+        'USER': 'root',
+        'PASSWORD': '2468evenMySql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
